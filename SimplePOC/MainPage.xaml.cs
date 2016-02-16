75b5a480930126073f3c413458a1b4a28d1e5bf5
ArturBK <artur.koefender@ilegra.com>
2016-02-16 21:51:18 -0200

    inicio

---
+++ SimplePOC/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SimplePOC
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+		public void AtualizaContador(Object sender, EventArgs e) {
+			double valor = stpContador.Value;
+			lblContador.Text = valor.ToString ();
+		}
+
+		public void salvaAlgo(Object sender, EventArgs e){
+			string descricao = itemQualquer.Text;
+
+			DisplayAlert ("alertando geral", descricao, "Ok");
+		}
+
+		public void IrPagna2(){
+			PaginaDois tela = new PaginaDois ();
+			Navigation.PushAsync (tela);
+		}
+
+		public void Clear(){
+			lblContador.Text = "";
+			stpContador.Value = 10;
+		}
+	}
+}
+
---
+++ SimplePOC/PaginaDois.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SimplePOC
+{
+	public partial class PaginaDois : ContentPage
+	{
+		public PaginaDois ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
